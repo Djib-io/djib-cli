90a0a1308f091cd54237e294dd64699bc89649b3
Aidin Mohandesi <Aidin.mohandesi@gmail.com>
2022-01-26 21:51:59 +0330

    adding Exception class declaration

--- libs/Exception.h
+++ libs/Exception.h
@@ -6,9 +6,37 @@
 #define DJIB_CLI_EXCEPTION_H
 
 
-class Exception {
+#include <exception>
+#include <string>
 
-};
+#include "../assets/Exception.h"
+
+namespace Djib {
+
+    class Exception : public std::exception {
+    public:
+        Exception() = delete;
+
+        Exception(const Exception &other) = delete;
+
+        Exception(Exception *&other) = delete;
+
+        ~Exception() = default;
+
+        explicit Exception(const std::string &msg);
+
+        virtual const char *what() const noexcept override;
+
+        const std::string &get_message() const;
+
+    protected:
+        void set_message(const std::string &msg);
+
+    private:
+        std::string __message;
+    };
+}
+
 
 
 #endif //DJIB_CLI_EXCEPTION_H
