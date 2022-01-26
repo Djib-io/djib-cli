18433f9ee413f54b2972309d91d4f7ad527f204a
Aidin Mohandesi <Aidin.mohandesi@gmail.com>
2022-01-26 22:16:34 +0330

    adding ProcessCaller exception declarations

--- libs/ProcessCaller.h
+++ libs/ProcessCaller.h
@@ -5,10 +5,30 @@
 #ifndef DJIB_CLI_PROCESSCALLER_H
 #define DJIB_CLI_PROCESSCALLER_H
 
+#include <string>
 
-class ProcessCaller {
+namespace Djib::Process {
 
-};
+    class ProcessNameError{
+    public:
+        ProcessNameError();
+    };
 
+    class ProcessNotFoundError {
+    public:
+        ProcessNotFoundError();
+    };
 
+    class ProcessFailedError {
+    public:
+        ProcessFailedError() = delete;
+
+        ProcessFailedError(const std::string &cause);
+    };
+
+    class ProcessCaller {
+
+    };
+
+}
 #endif //DJIB_CLI_PROCESSCALLER_H
