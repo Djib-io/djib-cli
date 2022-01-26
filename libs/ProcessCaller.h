7b4118f4a9e27032abf6f4063beb1254d3fc4976
Aidin Mohandesi <Aidin.mohandesi@gmail.com>
2022-01-26 22:15:28 +0330

    adding ProcessCaller class

---
+++ libs/ProcessCaller.h
@@ -0,0 +1,14 @@
+//
+// Created by themaqs on 1/26/22.
+//
+
+#ifndef DJIB_CLI_PROCESSCALLER_H
+#define DJIB_CLI_PROCESSCALLER_H
+
+
+class ProcessCaller {
+
+};
+
+
+#endif //DJIB_CLI_PROCESSCALLER_H
