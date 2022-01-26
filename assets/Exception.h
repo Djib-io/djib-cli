4be06acf96e32587886ff6924f24e7acd436fbd7
Aidin Mohandesi <Aidin.mohandesi@gmail.com>
2022-01-26 21:57:36 +0330

    adding Exception class definition; fixing Exception asset header macro definition

--- assets/Exception.h
+++ assets/Exception.h
@@ -2,15 +2,15 @@
 // Created by themaqs on 1/26/22.
 //
 
-#ifndef DJIB_CLI_EXCEPTION_H
-#define DJIB_CLI_EXCEPTION_H
+#ifndef DJIB_CLI_ASSETS_EXCEPTION_H
+#define DJIB_CLI_ASSETS_EXCEPTION_H
 
 #include <string>
 
 namespace Djib {
 
-    constexpr auto __djib_exception_prefix__ = "Djib returned an exception: ";
+    constexpr auto _djib_exception_prefix_ = "Djib returned an exception: ";
 
 }
 
-#endif //DJIB_CLI_EXCEPTION_H
+#endif //DJIB_CLI_ASSETS_EXCEPTION_H
--- libs/Exception.cpp
+++ libs/Exception.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Exception.h"
+
+using namespace Djib;
+
+Exception::Exception(const std::string &msg){
+    this->__message = std::string(_djib_exception_prefix_) + msg;
+}
+
+const std::string &Exception::get_message() const {
+    return this->__message;
+}
+
+const char *Exception::what() const noexcept {
+    return this->__message.c_str();
+}
+
+void Exception::set_message(const std::string &msg) {
+    this->__message = msg;
+}
