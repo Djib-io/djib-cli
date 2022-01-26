6c993614583678991a67914bf3089bc88800ffc9
Aidin Mohandesi <Aidin.mohandesi@gmail.com>
2022-01-26 22:22:29 +0330

    adding definitions of ProcessCaller properties

--- libs/ProcessCaller.h
+++ libs/ProcessCaller.h
@@ -6,9 +6,14 @@
 #define DJIB_CLI_PROCESSCALLER_H
 
 #include <string>
+#include <sstream>
+#include <boost/process.hpp>
 
 #include "Exception.h"
 #include "../assets/ProcessCaller.h"
+
+namespace bp = boost::process;
+namespace bf = boost::filesystem;
 
 namespace Djib::Process {
 
@@ -26,11 +31,24 @@ namespace Djib::Process {
     public:
         ProcessFailedError() = delete;
 
-        ProcessFailedError(const std::string &cause);
+        explicit ProcessFailedError(const std::string &cause);
     };
 
     class ProcessCaller {
+    public:
+        const std::string &get_process_name() const;
 
+        const std::string &get_output() const;
+
+        const std::string &get_params() const;
+
+        const std::string &get_process_path() const;
+
+    private:
+        std::string __process_name;
+        std::string __params;
+        std::string __output;
+        bf::path __process_path;
     };
 
 }
