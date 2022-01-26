a2d4e691cc0008a8e31f49c66618b8afb337cbcb
Aidin Mohandesi <Aidin.mohandesi@gmail.com>
2022-01-26 21:49:16 +0330

    adding Exception strings header;

---
+++ assets/Exception.h
@@ -0,0 +1,16 @@
+//
+// Created by themaqs on 1/26/22.
+//
+
+#ifndef DJIB_CLI_EXCEPTION_H
+#define DJIB_CLI_EXCEPTION_H
+
+#include <string>
+
+namespace Djib {
+
+    constexpr auto __djib_exception_prefix__ = "Djib returned an exception: ";
+
+}
+
+#endif //DJIB_CLI_EXCEPTION_H
